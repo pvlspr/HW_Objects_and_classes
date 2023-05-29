41aa95729853eb5af317903db97fa27a4b82da7a
PVL <pvlspr@ya.ru>
2023-05-29 18:35:10 +0300

    Add 1 task

---
+++ HOMEWORK.py
@@ -0,0 +1,67 @@
+class Student:
+    def __init__(self, name, surname, gender):
+        self.name = name
+        self.surname = surname
+        self.gender = gender
+        self.finished_courses = []
+        self.courses_in_progress = []
+        self.grades = {}
+
+    def rate_lec(self, student, course, grade):
+        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+            if course in student.grades:
+                student.grades[course] += [grade]
+            else:
+                student.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
+class Mentor:
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+        self.courses_attached = []
+
+class Lecturer(Mentor):
+    def __init_(self, name, surname, grade):
+        self.grades = {}
+    
+    def __str__(self):
+        some_lecturer = f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции: '
+        return some_lecturer
+
+class Reviewer(Mentor):
+    def rate_hw(self, student, course, grade):
+        if (isinstance(student, Student) and course in self.courses_attached
+            and course in student.courses_in_progress):
+            if course in student.grades:
+                student.grades[course] += [grade]
+            else:
+                student.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
+    def __str__(self):
+        some_reviewer = f'Имя: {self.name}\nФамилия: {self.surname}'
+        return some_reviewer
+
+best_student = Student('Ruoy', 'Eman', 'your_gender')
+best_student.courses_in_progress += ['Python']
+
+cool_mentor = Reviewer('Some', 'Buddy')
+cool_mentor.courses_attached += ['Python']
+
+cool_mentor.rate_hw(best_student, 'Python', 10)
+cool_mentor.rate_hw(best_student, 'Python', 10)
+cool_mentor.rate_hw(best_student, 'Python', 10)
+
+some_reviewer = Reviewer('Some', 'Buddy')
+
+some_lecturer = Lecturer('Some', 'Buddy')
+
+print(best_student.grades)
+print()
+print(some_reviewer)
+print()
+print(some_lecturer)
+print()
