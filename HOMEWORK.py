40d156706afd6f0feb43ecaa746f16e163f55fcb
PVL <pvlspr@ya.ru>
2023-05-30 13:56:01 +0300

    My finish

--- HOMEWORK.py
+++ HOMEWORK.py
@@ -8,7 +8,9 @@ class Student:
         self.grades = {}
 
     def rate_lec(self, student, course, grade):
-        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+        if (isinstance(student, Student)
+            and course in self.courses_attached
+            and course in student.courses_in_progress):
             if course in student.grades:
                 student.grades[course] += [grade]
             else:
